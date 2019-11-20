4cb8500ce1d572d9d5444303d0864d7bc91cc7a7
106051075 <55476779+106051075@users.noreply.github.com>
2019-11-20 15:57:28 +0800

    20191120

--- New Unity Project/Assets/Chicken.cs
+++ New Unity Project/Assets/Chicken.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Chicken : MonoBehaviour
 {
     // Start is called before the first frame update
+    [Header ("G8雞基本資料"),Tooltip ("Call me G8"),Range (1,100)]
+    public int speed = 10;
+    public float turn = 20.5f;
+    public string _Name = "G8雞";
+    public bool mission;
     void Start()
     {
         
---
+++ New Unity Project/Assets/learnmethod.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+
+public class learnmethod : MonoBehaviour
+{
+    private void Drive(int speed)
+    {
+        print("開車，時速:" + speed);
+        print("音效~");
+
+    }
+    private void Shoot(int count,float speed,string prop = "無",string direction = "前方")
+    {
+        print("弓箭數量:" + count);
+        print("弓箭速度:" + speed);
+        print("弓箭屬性:" + prop);
+        print("弓箭方向:" + direction);
+    }
+    private void Start()
+    {
+        print("哈囉，沃德~");
+        Drive(100);
+        Shoot(1, 15f, "冰屬性");
+        Shoot(2, 20f, direction: "全角度無死角掃射");
+    }
+}
