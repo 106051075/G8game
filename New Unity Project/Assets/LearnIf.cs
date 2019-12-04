5e92bd40cfb515c84c306d4aa34a9c32d322994d
106051075 <55476779+106051075@users.noreply.github.com>
2019-12-04 16:05:35 +0800

    1204

---
+++ New Unity Project/Assets/LearnIf.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class LearnIf : MonoBehaviour
+{
+    public bool opendoor;
+    public int combo;
+    public void Update()
+    {
+        if(opendoor)
+        {
+            print("開門");
+        }
+        else
+        {
+            print("關門");
+        }
+
+
+        if (combo >= 100) 
+        {
+            print("攻擊*10");
+        }
+        else if (combo >= 50) 
+        {
+            print("攻擊*5");
+        }
+        else if (combo >= 10) 
+        {
+            print("攻擊*2");
+        }
+        else 
+        {
+            print("攻擊*1");
+        }
+    }
+
+}
---
+++ New Unity Project/Assets/Learnoperator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Learnoperator : MonoBehaviour
+{
+    public int A = 10, B = 3;
+    public int C = 1;
+    public int D = 2, E = 4;
+    public int F = 5;
+    public bool key;
+    public int enemy;
+    public float HP;
+    private void Start()
+    {
+    }
+    private void Update()
+    {
+        print("死亡:" + (HP <= 0));
+        print("過關:" + (key || enemy >= 5));
+    }
+}
