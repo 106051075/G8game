3e308a462eb069884a602013813fff5e9d91ff1d
106051075 <55476779+106051075@users.noreply.github.com>
2020-01-07 02:03:15 +0800

    test

---
+++ New Unity Project (1)/Assets/NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int[] myArray;
+    public string[] secondArray;
+void Start()
+    {
+        myArray = new int[] {0,0};
+        secondArray = new[] { "0" };
+    }
+    void Update()
+    {
+        if (secondArray[0] == "+")       
+        print(myArray[0] + myArray[1]);
+        else if (secondArray[0] == "-")
+        print(myArray[0] - myArray[1]);
+        else if (secondArray[0] == "*")
+        print(myArray[0] * myArray[1]);
+
+    }
+}
---
+++ New Unity Project/Assets/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    int a, b;
+    string strA = "unity c#";
+    public string[] myArray;
+
+
+    void Start()
+    {
+
+    }
+}
+
+
